1dfc73a06f87fa82a88dc6c15e9a7e28ae0dde3a
duanledexianxianxian <fengyuchenglun@foxmail.com>
2020-05-19 15:40:11 +0800

    add doc

--- src/main/kotlin/com/kim/zilean/action/ZileanGenerationAction.kt
+++ src/main/kotlin/com/kim/zilean/action/ZileanGenerationAction.kt
@@ -12,7 +12,8 @@ class ZileanGenerationAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val data = e.getData(LangDataKeys.PSI_ELEMENT_ARRAY)
         // 表节点与数据源节点
-        if (data == null || (data.filterIsInstance<DbTable>().isEmpty() && data.filterIsInstance<DasNamespace>().isEmpty())) {
+//        if (data == null || (data.filterIsInstance<DbTable>().isEmpty() && data.filterIsInstance<DasNamespace>().isEmpty())) {
+        if (data == null || data.filterIsInstance<DbTable>().isEmpty()) {
             Messages.showMessageDialog("请至少选择一张数据表", "提示", Messages.getInformationIcon())
             return
         }
@@ -22,6 +23,7 @@ class ZileanGenerationAction : AnAction() {
     override fun update(e: AnActionEvent) {
         val data = e.getData(LangDataKeys.PSI_ELEMENT_ARRAY)
         // 表节点与数据源节点
-        e.presentation.isEnabledAndVisible = data != null && (data.filterIsInstance<DbTable>().isNotEmpty() || data.filterIsInstance<DasNamespace>().isNotEmpty())
+//        e.presentation.isEnabledAndVisible = data != null && (data.filterIsInstance<DbTable>().isNotEmpty() || data.filterIsInstance<DasNamespace>().isNotEmpty())
+        e.presentation.isEnabledAndVisible = data != null && (data.filterIsInstance<DbTable>().isNotEmpty())
     }
 }
